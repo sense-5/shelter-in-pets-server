1974650c55b53c13e6d4d88053d6464761be0fb7
Angela Vuong <vuong.angela@gmail.com>
2020-03-24 17:32:26 -0400

    placed getToken in utils file and have api route for single dog

--- server/api/allDogs.js
+++ server/api/allDogs.js
@@ -1,24 +1,33 @@
 const router = require('express').Router()
 const axios = require('axios')
+const {getToken} = require('../../utils')
 module.exports = router
-// ROUTE: '/api/dogs
-router.get('/', async (req, res, next) => {
+
+// ALL DOGS ROUTE: '/api/dogs
+// TODO: ask sarah about loading more dogs when making the request. Ask about SDK.
+router.get('/', getToken, async (req, res, next) => {
   try {
-    const token = await getToken()
-    console.log('token:', token)
-    res.json(token).status(200)
+    const {data} = await axios.get(
+      'https://api.petfinder.com/v2/animals?type=dog&limit=100&status=adoptable',
+      {headers: {Authorization: process.env.BEARER_TOKEN}}
+    )
+    console.log('get Data:', data)
+    res.json(data)
   } catch (error) {
     next(error)
   }
 })
-async function getToken() {
-  const {data} = await axios.post('https://api.petfinder.com/v2/oauth2/token', {
-    grant_type: 'client_credentials',
-    client_id: process.env.PETFINDER_KEY,
-    client_secret: process.env.PETFINDER_SECRET
-  })
-  const tokenObj = {}
-  tokenObj.token_type = data.token_type
-  tokenObj.access_token = data.access_token
-  return tokenObj
-}
+
+// SINGLE DOGS ROUTE: '/api/dogs/:dogId
+router.get('/:dogId', getToken, async (req, res, next) => {
+  try {
+    const {data} = await axios.get(
+      `https://api.petfinder.com/v2/animals/${req.params.dogId}`,
+      {headers: {Authorization: process.env.BEARER_TOKEN}}
+    )
+    console.log('get single dog data:', data)
+    res.json(data).status(200)
+  } catch (error) {
+    next(error)
+  }
+})
