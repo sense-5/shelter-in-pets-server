4e48dedff4f7e65eef97b664067edbff5ff64616
Angela Vuong <vuong.angela@gmail.com>
2020-03-24 15:59:11 -0400

    added getToken function to get bearer token from petfinder

--- server/api/allDogs.js
+++ server/api/allDogs.js
@@ -1,20 +1,24 @@
 const router = require('express').Router()
 const axios = require('axios')
 module.exports = router
-
 // ROUTE: '/api/dogs
 router.get('/', async (req, res, next) => {
   try {
-    const tokenRequest = await axios.post(
-      'https://api.petfinder.com/v2/oauth2/token',
-      {
-        grant_type: 'client_credentials',
-        client_id: process.env.PETFINDER_KEY,
-        client_secret: process.env.PETFINDER_SECRET
-      }
-    )
-    const allDogs = await axios.get()
+    const token = await getToken()
+    console.log('token:', token)
+    res.json(token).status(200)
   } catch (error) {
     next(error)
   }
 })
+async function getToken() {
+  const {data} = await axios.post('https://api.petfinder.com/v2/oauth2/token', {
+    grant_type: 'client_credentials',
+    client_id: process.env.PETFINDER_KEY,
+    client_secret: process.env.PETFINDER_SECRET
+  })
+  const tokenObj = {}
+  tokenObj.token_type = data.token_type
+  tokenObj.access_token = data.access_token
+  return tokenObj
+}
